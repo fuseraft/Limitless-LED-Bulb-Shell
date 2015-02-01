f769b77823e4d5fefd0366cbbaf28b94bb2f5329
Scott Christopher Stauffer <scstauf@gmail.com>
2015-02-01 03:11:28 -0600

    Minor refactoring
    bulb will be NULL if malloc() couldn't get any memory

--- bulb.c
+++ bulb.c
@@ -13,7 +13,6 @@
 #include <stdint.h> 
 #include <stdio.h>
 
-
 static const int LED_GATEWAY_PORT = 8899;
 
 static int _send_packet(const LEDBulb *bulb, char packet[]) {
@@ -23,36 +22,23 @@ static int _send_packet(const LEDBulb *bulb, char packet[]) {
 	return 0;
 }
 
-
-
 /* Create a new LED bulb object given IP address */
 LEDBulb* init_led_bulb(const char *ip_address) {
-
 	LEDBulb *bulb = malloc(sizeof(LEDBulb));
-	if(bulb == NULL) {
-		return NULL;
+	if(bulb != NULL) {
+		bulb->socket = socket(AF_INET,SOCK_DGRAM,0);
+		bulb->remote.sin_family = AF_INET;
+		bulb->remote.sin_addr.s_addr = inet_addr(ip_address);
+		bulb->remote.sin_port = htons(LED_GATEWAY_PORT);
+		bulb->is_connected = 1;
 	}
-
-
-	
-	bulb->socket = socket(AF_INET,SOCK_DGRAM,0);
-	bulb->remote.sin_family = AF_INET;
-	bulb->remote.sin_addr.s_addr = inet_addr(ip_address);
-	bulb->remote.sin_port = htons(LED_GATEWAY_PORT);
-	bulb->is_connected = 1;
-
 	return bulb;
-
 }
 
-
 int led_bulb_set_white(const LEDBulb *bulb){
-	
 	char packet[] = {0xc2,0x00,0x55};
 	_send_packet(bulb,packet);
 	return 0;
-	
-
 }
 
 // int led_bulb_off(LEDBulb *bulb);
