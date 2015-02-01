b15dadaf02ef7d7bdd6800a5b3145172817e272b
Scott Christopher Stauffer <scstauf@gmail.com>
2015-02-01 03:18:55 -0600

    Added null check
    Prevent a segfault with a null check

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ int main()
 {
 	printf("Starting.... newwww\n");
 	LEDBulb *test = init_led_bulb("192.168.0.14");
-	printf("is connected : %d\n",test->is_connected);
-	//led_bulb_set_color(test, GREEN);
-	//sleep(5000);
-	led_bulb_set_white(test);
-
-
-
-
+	if (test != NULL) {
+		printf("is connected : %d\n",test->is_connected);
+		//led_bulb_set_color(test, GREEN);
+		//sleep(5000);
+		led_bulb_set_white(test);
+	} 
+	else {
+		printf("could not connect to 192.168.0.14");
+	}
 	return 0;
-
 }
